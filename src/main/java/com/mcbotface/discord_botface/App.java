95cbde65210f59211976222443ab01f15b5c3d34
tcronis <46980360+tcronis@users.noreply.github.com>
2019-12-13 18:37:15 -0600

    Created the Maven Project

---
+++ src/main/java/com/mcbotface/discord_botface/App.java
@@ -0,0 +1,13 @@
+package com.mcbotface.discord_botface;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
