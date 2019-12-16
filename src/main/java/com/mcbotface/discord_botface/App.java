bead4b3b3cd44e484721fcd5965ebe54e0503eb7
tcronis <46980360+tcronis@users.noreply.github.com>
2019-12-15 21:37:50 -0600

    Creation of the authenticator for grabbing the discord token

--- src/main/java/com/mcbotface/discord_botface/App.java
+++ src/main/java/com/mcbotface/discord_botface/App.java
@@ -1,13 +1,53 @@
 package com.mcbotface.discord_botface;
 
+import javax.security.auth.login.LoginException;
+
+import net.dv8tion.jda.api.AccountType;
+import net.dv8tion.jda.api.JDABuilder;
+import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
+import net.dv8tion.jda.api.hooks.ListenerAdapter;
+import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed.ImageInfo;
+import net.dv8tion.jda.api.events.message.MessageEmbedEvent;
+
 /**
  * Hello world!
  *
  */
-public class App 
+public class App extends ListenerAdapter
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws Exception
     {
-        System.out.println( "Hello World!" );
+    	
+        JDABuilder builder = new JDABuilder(AccountType.BOT);
+        Authenticator authen = new Authenticator();				//call the custom authenticator class to get token
+        builder.setToken(authen.getToken());
+        builder.addEventListeners(new App());
+        builder.build();
+    }
+    
+    @Override
+    public void onMessageReceived(MessageReceivedEvent event) {
+    	if(event.getMessage().getAuthor().isBot())
+    		return;
+    	if(event.getMessage().getContentDisplay().toString().charAt(0) == '!') {
+    		String command = event.getMessage().getContentDisplay().toString();
+    		//!emoji call
+    		
+    		//!self destruct call
+    		
+    		
+    	}
+    	
+    	//F reaction after someone put F in the chat
+    	
+    	
+    	System.out.println("recieved the message");
+    	System.out.println(event.getAuthor());
+    	System.out.println(event.getMessage().getContentDisplay());
+    	
+    	
+    	
     }
 }
