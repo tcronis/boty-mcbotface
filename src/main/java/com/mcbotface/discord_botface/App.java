702a16b3a921e353ec5f73e28d738dadd13a8fa9
tcronis <46980360+tcronis@users.noreply.github.com>
2020-01-07 16:12:36 -0600

    Adding the insults to the bot as removing some other non-implemented features

--- src/main/java/com/mcbotface/discord_botface/App.java
+++ src/main/java/com/mcbotface/discord_botface/App.java
@@ -1,11 +1,15 @@
 package com.mcbotface.discord_botface;
 
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
+import java.util.Random;
 import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileNotFoundException;
 import java.io.FileReader;
-
+import java.time.format.DateTimeFormatter;
+import java.time.LocalDateTime;
 import javax.security.auth.login.LoginException;
 
 import net.dv8tion.jda.api.AccountType;
@@ -13,20 +17,17 @@ import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.MessageEmbed.ImageInfo;
 import net.dv8tion.jda.api.entities.MessageHistory;
+import net.dv8tion.jda.api.entities.User;
 import net.dv8tion.jda.api.events.message.MessageEmbedEvent;
 
-/**
- * Hello world!
- *
- */
 public class App extends ListenerAdapter
 {
     public static void main( String[] args ) throws Exception
-    {
-    	
+    { 
         JDABuilder builder = new JDABuilder(AccountType.BOT);
         Authenticator authen = new Authenticator();				//call the custom authenticator class to get token
 //        builder.setStatus(OnlineStatus.OFFLINE);
@@ -40,6 +41,8 @@ public class App extends ListenerAdapter
     	if(event.getMessage().getAuthor().isBot())
     		return;
     	if(event.getMessage().getContentDisplay().toString().charAt(0) == '!') {
+    		//strip spaces recursively
+    		
     		Boolean space = event.getMessage().getContentDisplay().contains(" ");
     		Boolean attachments = (event.getMessage().getAttachments().size() >= 1 ? true : false);
     		String command;
@@ -47,36 +50,97 @@ public class App extends ListenerAdapter
     			command = event.getMessage().getContentDisplay().toString().substring(1, event.getMessage().getContentDisplay().indexOf(" "));
     		else
     			command = event.getMessage().getContentDisplay().toString().substring(1);
+    		
     		//!create_emoji-NAMEOFEMOJI - will create an emoji based on the photo given
     		if(command.toLowerCase().contains("create_emoji")) {
-    			Boolean spaces = (event.getMessage().getContentDisplay().contains(" "));
-    			Boolean name_sep = event.getMessage().getContentDisplay().contains("-");
-    			if(event.getMessage().getContentDisplay().substring(event.getMessage().getContentDisplay().indexOf("-") + 1).length() > 0) {
-    				String name = "";
-        			if(spaces)
-        				name = event.getMessage().getContentDisplay().substring(event.getMessage().getContentDisplay().indexOf("-") + 1, event.getMessage().getContentDisplay().indexOf(" "));
-        			else
-        				name = event.getMessage().getContentDisplay().substring(event.getMessage().getContentDisplay().indexOf("-") + 1);
-        			Emoji_Creator emoji_creation = new Emoji_Creator(event, name);
-    			}else
-    				event.getChannel().sendMessage("Hey the dickhead named " + event.getAuthor() + " you need to include a file name, refer to the '!i_am_a_smooth_brain' help screen");
-    			
-    		}
-    		//!self_destruct_mcbotface - will exit the server 
-    		else if(command.equalsIgnoreCase("self_destruct_mcbotface")) {
-    			event.getChannel().sendMessage("Good bye cruel world :(");
-    			System.exit(0);
+    			String emoji_name = "";
+    			try {
+    				//substring the emoji name and then remove all non-alphabetical characters
+    				emoji_name = event.getMessage().getContentDisplay().substring(event.getMessage().getContentDisplay().indexOf("-") + 1);
+    				emoji_name = emoji_name.replaceAll("[^a-zA-Z]", "");
+    				if(emoji_name.length() < 2) {
+    					event.getChannel().sendMessage("Chief, your emoji name must be larger than 2 characters").queue();
+    					throw new Exception("Too small of an emoji name");
+    				}
+    				if(emoji_name.length() > 32) {
+    					event.getChannel().sendMessage("Chief, you emoji name can't be longer than 32 chars");
+    					throw new Exception("Too large of an emoji name");
+    				}
+    			}catch(Exception e){
+    				event.getChannel().sendMessage("Chief, looks like there was an error in your emoji name. Remember must be" +
+    							"it must be at least 2 characters long and only alphabetical characters").queue();
+    				e.printStackTrace();
+    			}
+    			try {
+    				new Emoji_Creator(event, emoji_name);
+    			} catch(Exception e) {
+    				System.out.println("Error occured when creating the emoji object");
+    				e.printStackTrace();
+    			}
     		}
     		//!execute_order_66 - kill all the bots on the server
     		else if(command.equalsIgnoreCase("execute_order_66")) {
-    			
+    			//get a list of the members of the discord and find the bots, then kick them off the server
+    			List <Member> users = event.getGuild().getMembers();
+    			Boolean kicked = false;
+    			for(Member you : users) {
+    				if(you.getUser().isBot()) {
+    					System.out.println("attempting to kick - " + you.getUser().getName());
+    					event.getChannel().sendMessage("Cya later fucko <@" + you.getId() + ">").queue();
+    					event.getGuild().kick(you.getId()).queue();
+    					kicked = true;
+    				}
+    			}
+    			if(kicked) {
+    				event.getChannel().sendMessage("Damn it feels good to get all dem BOTS out of here, amirights?");
+    			}
     		}
-    		//!murder_INSERTBOTNAMEHERE - remove the bot by this name off the sever
-    		else if(command.contains("murder_")) {
-    			
+    		//!murder_INSERT_AT_NAME - remove user by this name off the sever
+    		else if(command.contains("murder")) {
+    			List <Member> users_to_kick = event.getMessage().getMentionedMembers();
+    			if(users_to_kick.size() > 0) {
+    				event.getChannel().sendMessage("I would delighted to kick them").queue();
+    				for(Member m : users_to_kick) {
+    					event.getGuild().kick(m).queue();
+    				}
+    			}else {
+    				event.getChannel().sendMessage("Listen man, you need to learn how to use my commands. Refer to my commands list on how to kick other bots/members. This is your first and list warning.").queue();
+    			}
     		}
-    		else if(command.equalsIgnoreCase("i_am_a_smooth_brain")) {    			
+    		else if(command.equalsIgnoreCase("i_am_a_smooth_brain") || command.equalsIgnoreCase("help")) {    			
     			event.getChannel().sendMessage(getCommandsTextFile()).queue();
+    		}
+    		else if(command.equalsIgnoreCase("insult")){
+    			if(event.getMessage().getMentionedMembers().size() > 0) {
+    				event.getMessage().getMentionedMembers().forEach((Member m) -> {
+    					try {
+    						File insults = new File("insults.txt");
+							BufferedReader reader = new BufferedReader(new FileReader(insults));
+							int num_insults_in_file = Integer.valueOf(reader.readLine());
+							int location = new Random().nextInt(num_insults_in_file) + 1;	//first line is the number of insults, shift by 1 
+							int count = 0;
+							while(count < (location - 1)) {reader.readLine(); count++;}
+							String insult_to_say = reader.readLine();
+							reader.close();
+							event.getChannel().sendMessage("<@" + m.getId() + "> " + insult_to_say).queue();
+						} catch (Exception e) {e.printStackTrace();}
+    					
+    				});
+    			}else {
+    				try {
+    					File insults = new File("insults.txt");
+						BufferedReader reader = new BufferedReader(new FileReader(insults));
+						int num_insults_in_file = Integer.valueOf(reader.readLine());
+						int location = new Random().nextInt(num_insults_in_file) + 1;	//first line is the number of insults, shift by 1 
+						int count = 0;
+						while(count < (location - 1)) {reader.readLine(); count++;}
+						String insult_to_say = reader.readLine();
+						reader.close();
+						event.getChannel().sendMessage("<@" + event.getMessage().getAuthor().getId() + "> " + insult_to_say).queue();
+    				}catch(Exception e) {
+    					e.printStackTrace();
+    				}
+    			}
     		}
     		else {
     			event.getChannel().sendMessage("I don't know what the fuck you meant to say, but "+ 
@@ -84,7 +148,7 @@ public class App extends ListenerAdapter
     		}
     	}
     	
-    	//F reaction after someone put F in the chat    	
+
     }
     
     //loading a the commands text file for it to be displayed in the  discord
@@ -101,7 +165,7 @@ public class App extends ListenerAdapter
         	reader.close();
     	}catch(Exception e) {
     		System.out.println("Error when reading in the commands text file");
-    		file = "**Well some FUCK FACE didn't put the commands.txt file in my sights, so I dont' know what to tell you. Go look me up on github https://github.com/tcronis/boty-mcbotface";
+    		file = "**Well some FUCK FACE didn't put the commands.txt file in my sighxts, so I dont' know what to tell you. Go look me up on github https://github.com/tcronis/boty-mcbotface";
     		e.printStackTrace();
     	}
     	return file;
--- src/main/java/com/mcbotface/discord_botface/Emoji_Creator.java
+++ src/main/java/com/mcbotface/discord_botface/Emoji_Creator.java
@@ -2,37 +2,24 @@ package com.mcbotface.discord_botface;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileReader;
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.awt.Graphics2D;
-
 import javax.imageio.ImageIO;
-import javax.security.auth.login.LoginException;
-import javax.swing.ImageIcon;
-
-import net.dv8tion.jda.api.AccountType;
-import net.dv8tion.jda.api.JDABuilder;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
-import net.dv8tion.jda.api.hooks.ListenerAdapter;
-import net.dv8tion.jda.api.*;
+import net.dv8tion.jda.api.entities.Emote;
 import net.dv8tion.jda.api.entities.Icon;
-import net.dv8tion.jda.api.entities.Message;
 import net.dv8tion.jda.api.entities.Message.Attachment;
-import net.dv8tion.jda.api.entities.MessageEmbed.ImageInfo;
-import net.dv8tion.jda.api.entities.MessageHistory;
-import net.dv8tion.jda.api.entities.Role;
-import net.dv8tion.jda.api.events.message.MessageEmbedEvent;
+
 
 
 public class Emoji_Creator {
 	private MessageReceivedEvent event;
 	private String emoji_name;
+	
+	Emoji_Creator(MessageReceivedEvent event){
+		this.event = event;
+	}
 	
 	Emoji_Creator(MessageReceivedEvent e, String name){
 		this.event = e;
@@ -51,43 +38,37 @@ public class Emoji_Creator {
 						a.downloadToFile();
 						File f = new File(a.getFileName());
 						while(!f.exists()) {
-							System.out.println("Sleeping");
 							Thread.sleep(2000);
 						}
 						repeat_resize(f, a.getFileExtension());
-						System.out.println("Image resized to needed size");
-						add_emoji(f);
-						System.out.println("added emoji");
+						add_emoji(f, emoji_name);
 						f.delete();
-//						event.getGuild().createEmote(name, icon, roles);
 					} catch (Exception e) {
 						e.printStackTrace();
 					}
-
 				}
 			}
-			
-		}else {
-			
+		}
+		else {
+			event.getChannel().sendMessage("<@"+event.getAuthor().getId()+">, everyone alread knows you're special, "+
+						"there is no need to fuck my these commands smooth brain, refer to the help guide.");
 		}	
 	}
-	private void add_emoji(File img) {
+	//MAX_NAME_IS 32 MIN_NAME_IS 2
+	private void add_emoji(File img, String name) {
 		try {
-			System.out.println(img.getName());
 			Icon ic = Icon.from(img);
-			System.out.println(emoji_name);
-			event.getGuild().createEmote(emoji_name, ic).complete();
+			event.getGuild().createEmote(name, ic).complete();
 		}catch(Exception e) {
+			event.getChannel().sendMessage("ERROR - I couldn't add the emoji to the server ¯\\_(ツ)_/¯").queue();
 			e.printStackTrace();
 		}
 	}
 	
-	
+	//recursively resizing the image if needed
 	private void repeat_resize(File a, String ext) {
-		System.out.println("repeat_resize");
 		try {
 			if(!checkSize(a)) {
-				System.out.println("Resizing image - ");
 				BufferedImage b = ImageIO.read(a);
 				int type = b.getType() == 0? BufferedImage.TYPE_INT_ARGB : b.getType();
 				BufferedImage resizedBuffImg = new BufferedImage((int) Math.round(b.getWidth() * 0.25), (int)Math.round(b.getHeight() * 0.25), type);
@@ -101,28 +82,18 @@ public class Emoji_Creator {
 			
 		}catch (Exception e) {
 			e.printStackTrace();
+			event.getChannel().sendMessage("ERROR - I failed on resizing the image ¯\\_(ツ)_/¯");
 		}
 	}
-	
+	//checking the size of the image, since it can't be greater than 256 kb
 	private Boolean checkSize(File img) {
-		System.out.println("checkSize");
 		if(img != null && img.exists()) {
-			System.out.println("Checking file size - " + img.length());
 			if((img.length() / 1024) <= 256)
 				return true;
 		}
 		return false;
 	}
 	
-	
-	private String find__old_message_url() {
-		String url = "";
-		
-		
-		
-		
-		
-		return url;
+	public void removeEmoji(long emoji_id) {
 	}
-	
 }
